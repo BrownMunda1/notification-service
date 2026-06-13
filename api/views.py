711e6ac5ac697ea5a56c39efe4a3803d206e5fb8
BrownMunda1 <ankitchopra6396@gmail.com>
2026-06-13 17:47:45 +0530

    Inital setup for notifiication service

---
+++ api/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseNotAllowed
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from api.serializers import NotificationSerializer
+
+@api_view(["POST"])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def notify(request):
+
+    print("Type of request:", type(request))
+
+    if request.method != "POST":
+        return HttpResponseNotAllowed(permitted_methods=["POST"])
+
+    serializer = NotificationSerializer(data=request.data)
+    try:
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data={"message": "Invalid Request Body"}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response(data={"error_message": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
